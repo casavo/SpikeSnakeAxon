464e0376a4009bc5e0b11d69e12d8b5ec6f7ee46
Massimiliano Ceriani <massimiliano.ceriani@casavo.com>
2021-11-24 14:03:08 +0100

    build: added quarkus

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "1.6.0"
+    kotlin("plugin.allopen") version "1.6.0"
+    id("io.quarkus") version "2.4.0.Final"
+    id("de.undercouch.download") version "4.1.2"
 }
 
 group = "com.casavo"
@@ -7,9 +10,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
+
 }
 
-val javaVersion = JavaVersion.VERSION_17
+val javaVersion = JavaVersion.VERSION_11
 java {
     sourceCompatibility = javaVersion
     targetCompatibility = javaVersion
@@ -23,6 +28,15 @@ dependencies {
     implementation("org.axonframework", "axon-eventsourcing")
     implementation("org.axonframework", "axon-messaging")
     implementation("org.axonframework", "axon-modelling")
+    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:2.4.0.Final"))
+    implementation("io.quarkus:quarkus-rest-client-jackson")
+    implementation("io.quarkus:quarkus-rest-client")
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("io.quarkus:quarkus-resteasy")
+    implementation("io.quarkus:quarkus-resteasy-jackson")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.quarkus:quarkus-arc")
+    implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", "2.13.0")
 
     testImplementation("org.axonframework", "axon-test")
     testImplementation(platform("org.junit:junit-bom:5+"))
@@ -30,8 +44,13 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine")
     testImplementation("org.hamcrest", "hamcrest", "2.2")
     testImplementation("org.mockito:mockito-core:4.1.0")
-
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = javaVersion.toString()
+    kotlinOptions.javaParameters = true
+}
+
 tasks {
     test {
         useJUnitPlatform()
--- src/main/kotlin/spikeSnakeAxon/domain/service/EvaService.kt
+++ src/main/kotlin/spikeSnakeAxon/domain/service/EvaService.kt
@@ -1,9 +1,12 @@
 package spikeSnakeAxon.domain.service
 
+import javax.enterprise.context.ApplicationScoped
+
 interface EvaluationService{
     fun valuateProperty(propertyData: Any): Double
 }
 
+@ApplicationScoped
 class EvaService: EvaluationService {
     override fun valuateProperty(propertyData: Any) = Math.random() * 100
 }
