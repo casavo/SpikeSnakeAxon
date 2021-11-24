672d9a0abf06bfc8963ac11e3ce96c24b2c35a05
Massimiliano Ceriani <massimiliano.ceriani@casavo.com>
2021-11-24 17:38:03 +0100

    Revert "build: added quarkus"
    This reverts commit 464e0376a4009bc5e0b11d69e12d8b5ec6f7ee46.

--- src/main/kotlin/spikeSnakeAxon/domain/service/EvaService.kt
+++ src/main/kotlin/spikeSnakeAxon/domain/service/EvaService.kt
@@ -1,12 +1,9 @@
 package spikeSnakeAxon.domain.service
 
-import javax.enterprise.context.ApplicationScoped
-
 interface EvaluationService{
     fun valuateProperty(propertyData: Any): Double
 }
 
-@ApplicationScoped
 class EvaService: EvaluationService {
     override fun valuateProperty(propertyData: Any) = Math.random() * 100
 }
