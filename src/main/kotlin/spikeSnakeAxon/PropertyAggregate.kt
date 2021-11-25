5f02114c00aed3738503576e996e24b251d9de02
Massimiliano Ceriani <massimiliano.ceriani@casavo.com>
2021-11-25 13:47:02 +0100

    Revert "add: WIP springboot and JDBC"
    This reverts commit 8bde50c202a278dc5a20fc3dd78c0950637d9cfd.

--- src/main/kotlin/spikeSnakeAxon/PropertyAggregate.kt
+++ src/main/kotlin/spikeSnakeAxon/PropertyAggregate.kt
@@ -1,12 +1,17 @@
 package spikeSnakeAxon
 
 import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.commandhandling.gateway.CommandGateway
+import org.axonframework.eventhandling.EventHandler
 import org.axonframework.eventsourcing.EventSourcingHandler
 import org.axonframework.modelling.command.AggregateIdentifier
 import org.axonframework.modelling.command.AggregateLifecycle.apply
 import org.axonframework.spring.stereotype.Aggregate
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import spikeSnakeAxon.CreatePropertyCommand
+import spikeSnakeAxon.EvaluatePropertyCommand
+import spikeSnakeAxon.EvaluationService
+import spikeSnakeAxon.PropertyCreatedEvent
+import spikeSnakeAxon.PropertyValuated
 import java.util.*
 
 @Aggregate
@@ -18,27 +23,23 @@ class PropertyAggregate {
     private lateinit var propertyData: Any
     private var valuation: Double? = null
 
-    final val logger: Logger = LoggerFactory.getLogger(PropertyAggregate::class.java)
 
     constructor()
 
     @CommandHandler
     constructor(command: CreatePropertyCommand) {
-        logger.info("CommandHandler - CreatePropertyCommand ")
         val aggregateId = command.propertyId
         apply(PropertyCreatedEvent(aggregateId, command.zipCode, command.data))
     }
 
     @CommandHandler
     fun handle(command: EvaluatePropertyCommand, evaluationService: EvaluationService){
-        logger.info("CommandHandler - EvaluatePropertyCommand ")
         val valuation = evaluationService.valuateProperty(propertyData)
         apply(PropertyValuated(command.propertyId, valuation))
     }
 
     @EventSourcingHandler
     fun on(event: PropertyCreatedEvent) {
-        logger.info("EventSourcingHandler - PropertyCreatedEvent ")
         propertyId = event.propertyId
         zipCode = event.zipCode
         propertyData = event.data
@@ -46,7 +47,6 @@ class PropertyAggregate {
 
     @EventSourcingHandler
     fun on(event: PropertyValuated) {
-        logger.info("EventSourcingHandler - PropertyValuated ")
         valuation = event.valuation
     }
 
--- src/main/kotlin/spikeSnakeAxon/ValuatePropertyPolicy.kt
+++ src/main/kotlin/spikeSnakeAxon/ValuatePropertyPolicy.kt
@@ -2,19 +2,14 @@ package spikeSnakeAxon
 
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.eventhandling.EventHandler
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class ValuatePropertyPolicy {
 
-    final val logger: Logger = LoggerFactory.getLogger(PropertyAggregate::class.java)
-
     @EventHandler
     fun policy(event: PropertyCreatedEvent, commandGateway: CommandGateway) {
-        logger.info("EventHandler - PropertyCreatedEvent")
         val command = EvaluatePropertyCommand(event.propertyId)
-//        commandGateway.send<EvaluatePropertyCommand>(command)
+        commandGateway.send<EvaluatePropertyCommand>(command)
     }
 }
